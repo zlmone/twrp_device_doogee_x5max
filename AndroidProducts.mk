46a5f2d1c49151e7634176a5579a2ec29ab0846d
zoggn <kurkov.nikita@gmail.com>
2018-10-05 22:39:08 +0300

    clean and fix some errors on android 8.1

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/w370.mk \
     $(LOCAL_DIR)/omni_w370.mk
--- omni_w370.mk
+++ omni_w370.mk
@@ -1,5 +1,7 @@
-# Inherit device configuration
-$(call inherit-product, device/doogee/w370/w370.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := w370
